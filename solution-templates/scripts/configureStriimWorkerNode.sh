a4b25e88f0a209b8ec007a73d2104136dd71d957
Deepak Garg <deepak@striim.com>
2018-03-29 00:54:06 -0700

    DEV-13456 added missing properties

--- solution-templates/scripts/configureStriimWorkerNode.sh
+++ solution-templates/scripts/configureStriimWorkerNode.sh
@@ -72,7 +72,8 @@ echo "LicenceKey=$WA_LICENSE_KEY" >> $STARTUP_PROPERTIES_FILE
 echo "DeploymentGroups=default" >> $STARTUP_PROPERTIES_FILE
 echo "ServerFqdn=$VM_FQDN" >> $STARTUP_PROPERTIES_FILE
 echo "NodePublicAddress=$WA_NODE_PUBLIC_IP" >> $STARTUP_PROPERTIES_FILE
-
+echo "ServerNodeAddress=$WA_NODE_PUBLIC_IP" >> $STARTUP_PROPERTIES_FILE
+echo "IsTcpIpCluster=true" >> $STARTUP_PROPERTIES_FILE
 
 cat << EFHOST > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
